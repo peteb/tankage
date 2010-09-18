268c4fc76822ad6e8a4515f189112749cc27a3d4
Peter Backman <ptr.bckmn@gmail.com>
2010-09-18 17:42:54 +0200

    bla

---
+++ src/hid_controller.h
@@ -0,0 +1,12 @@
+/*
+ * Snail Wail
+ * 
+ * (c) Copyright 2010 Peter Backman. All Rights Reserved. 
+ */
+
+#ifndef HID_CONTROLLER_H_49H4IYH7
+#define HID_CONTROLLER_H_49H4IYH7
+
+
+
+#endif /* end of include guard: HID_CONTROLLER_H_49H4IYH7 */
