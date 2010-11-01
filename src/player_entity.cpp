c7c11041973fcf0d593ff31e97efd7c937ae405b
Peter Backman <ptr.bckmn@gmail.com>
2010-11-01 17:53:40 +0100

    polished shooting a bit

--- src/player_entity.cpp
+++ src/player_entity.cpp
@@ -29,13 +29,13 @@ void PlayerEntity::setTarget(const Ref<Physics::Body> & newTarget) {
 void PlayerEntity::shoot() {
    boost::shared_ptr<Bullet> bullet = boost::dynamic_pointer_cast<Bullet>(creator.createObject("bullet", creator));
    bullet->setPosition(getPosition());
-   bullet->body->addImpulse(vec2(300.0f, 0.0f));
+   bullet->body->addImpulse(vec2(1000.0f, 0.0f));
    
-//    if (Ref<Physics::Body>::SharedPtr lockedTarget = target.lock())
-// 	   lockedTarget->addImpulse(vec2(-10.0f, 0.0f));
+   if (Ref<Physics::Body>::SharedPtr lockedTarget = target.lock())
+	  lockedTarget->addImpulse(vec2(-100.0f, 0.0f));
 
    bullet->shooter = shooterId;
-    world.insert(bullet);
+   world.insert(bullet);
    //world.add(bullet);
 }
 
@@ -62,29 +62,30 @@ void PlayerEntity::update(float dt) {
    }
    
    
-   if (Ref<Physics::Body>::SharedPtr acquiredTarget = target.lock()) {
-      vec2 pos = acquiredTarget->getPosition();
+   /*  if (Ref<Physics::Body>::SharedPtr acquiredTarget = target.lock()) {
+	  } */
+
+   if (Ref<Physics::Body>::SharedPtr lockedTarget = target.lock()) {
+      vec2 pos = lockedTarget->getPosition();
       pos.x = std::max(pos.x, -10.0f);
       pos.x = std::min(pos.x, 890.0f);
       pos.y = std::max(pos.y, 10.0f);
       pos.y = std::min(pos.y, 600.0f - 10.0f);
-
 
       vec2 delta = vec2(xPos, pos.y) - pos;
 
       if (delta.getMagnitude() > 50.0f) {
          delta.normalize();
          delta = delta * 50.0f;
-      }
-         
-	  // TODO: hur får man rätt impulse här?
-	  acquiredTarget->addImpulse(delta * 0.01f);
-	  //   acquiredTarget->setPosition(pos + delta * 5.0f * dt);
-   }   
+	  }
+
+	  lockedTarget->addImpulse(-lockedTarget->getVelocity() * 0.005f);
+	  lockedTarget->setPosition(pos + delta * 5.0f * dt);
+   }
 }
 
 void PlayerEntity::setPosition(const vec2 & newPos) {
-	if (Ref<Physics::Body>::SharedPtr acquiredTarget = target.lock())
+   if (Ref<Physics::Body>::SharedPtr acquiredTarget = target.lock())
       acquiredTarget->setPosition(newPos);
 }
 
