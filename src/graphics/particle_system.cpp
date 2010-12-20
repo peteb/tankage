868a6f397861792b449fc7064a7295ab17fa0fea
Peter Backman <ptr.bckmn@gmail.com>
2010-12-20 20:28:55 +0100

    Started working on the particle system.

---
+++ src/graphics/particle_system.cpp
@@ -0,0 +1,8 @@
+/*
+ * Snail Wail
+ * 
+ * (c) Copyright 2010 Peter Backman. All Rights Reserved. 
+ */
+
+#include "particle_system.h"
+
---
+++ src/graphics/particle_system.h
@@ -0,0 +1,23 @@
+/*
+ * Snail Wail
+ * 
+ * (c) Copyright 2010 Peter Backman. All Rights Reserved. 
+ */
+
+#ifndef GRAPHICS_PARTICLE_SYSTEM_H
+#define GRAPHICS_PARTICLE_SYSTEM_H
+
+namespace Graphics {
+
+
+class ParticleSystem {
+public:
+   
+
+};
+
+
+
+}
+
+#endif // !GRAPHICS_PARTICLE_SYSTEM_H
