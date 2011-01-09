1b34ff0f91753082a76de25ac4446a064d5c81ca
Peter Backman <ptr.bckmn@gmail.com>
2011-01-09 15:42:34 +0100

    the first snail is now getting rendered

--- src/game/snails.cpp
+++ src/game/snails.cpp
@@ -1,19 +1,52 @@
 #include <game/snails.h>
+#include <engine/graphics.h>
+#include <engine/texture.h>
+#include <engine/image_loader.h>
+#include <engine/portal.h>
+#include <engine/image.h>
+#include <utils/rect.h>
 
 Snails::Snails(const class Portal &interfaces, SystemContext *ctx)
   : System(ctx)
 {
+  graphics = interfaces.requestInterface<Graphics>();
+  ImageLoader *imgLoader = interfaces.requestInterface<ImageLoader>();
 
+  std::auto_ptr<Image> img(imgLoader->loadImage("../data/snail_l.png"));
+  Snail *snail = new Snail(vec2(50.0f, 300.0f));
+  snail->setTexture(graphics->createTexture(img.get()));
+
+  snails.push_back(snail);
 }
 
 void Snails::render() {
-
+  SnailVector::iterator i = snails.begin(), e = snails.end();
+  for (; i != e; ++i) {
+    (*i)->render(graphics);
+  }
 }
 
 Snail *Snails::snail(int id) const {
-  return new Snail;
+  return snails.at(id);
+}
+
+Snail::Snail(const vec2 &initialPos)
+  : position(initialPos)
+{
 }
 
 void Snail::startState(SnailState state) {
   
+}
+
+void Snail::setTexture(Texture *texture) {
+  this->texture = texture;
+}
+
+void Snail::render(Graphics *graphics) {
+  graphics->setBlend(Graphics::BLEND_ALPHA);
+  graphics->enableTextures();
+  texture->bind();
+
+  graphics->drawQuad(rect(position, 64, 64));
 }
--- src/game/snails.h
+++ src/game/snails.h
@@ -2,6 +2,8 @@
 #define GAME_SNAILS_H
 
 #include <game/system.h>
+#include <utils/vec.h>
+#include <vector>
 
 class Snail {
 public:
@@ -9,8 +11,17 @@ public:
     STATE_MOVE_UP,
     STATE_MOVE_DOWN
   };
+
+  Snail(const vec2 &initialPos);
   
   void startState(SnailState state);
+  void render(class Graphics *graphics);
+  void setTexture(class Texture *texture);
+  
+private:
+  class Texture *texture;
+  vec2 position;
+  
 };
 
 class Snails : public System {
@@ -24,6 +35,12 @@ public:
   
   void render();
   Snail *snail(int id) const;
+
+private:
+  class Graphics *graphics;
+
+  typedef std::vector<Snail *> SnailVector;
+  SnailVector snails;
 };
 
 #endif // !GAME_SNAILS_H
