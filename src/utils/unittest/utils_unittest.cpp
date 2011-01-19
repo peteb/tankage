dd8e7a7aabd21a2f4b4053ede35dc45fe6c5d29d
Kaspars Skels <kskels@gmail.com>
2011-01-19 21:47:34 +0100

    Unit test for mat2 class

--- src/utils/unittest/utils_unittest.cpp
+++ src/utils/unittest/utils_unittest.cpp
@@ -9,8 +9,7 @@
  * save a lot of code lines, is it good enough, though?
  * Internal ss rounding there finds difference between 3.0f and 3.00001f which
  * seems to be enough, also it is epsilon absolute error 0.00001 mentioned in
- * http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm
- */
+ * http://www.cygnus-software.com/papers/comparingfloats/comparingfloats.htm */
 TEST(utils_unittest, test_vec)
 {
     // constructors
@@ -40,6 +39,7 @@ TEST(utils_unittest, test_vec)
     EXPECT_EQ(std::string(vec2::Identity()), "0, 0");
 } // test_vec
 
+/** COMMENT kaspars: Identity and Zero probably are too obvious to test */
 TEST(utils_unittest, test_mat)
 {
 	vec2 v1(1.1f, 2.2f), v2(3.0f, 4.0f);
@@ -56,6 +56,30 @@ TEST(utils_unittest, test_mat)
 	EXPECT_FLOAT_EQ(2.4f, m2.elements[1][1]);	
 
 	v1 = m1 * v2;	
-	//EXPECT_FLOAT_EQ();
+	EXPECT_FLOAT_EQ(v1.x, 3.0f*1.1f+4.0f*3.0f);
+	EXPECT_FLOAT_EQ(v1.y, 3.0f*2.2f+4.0f*4.0f);
 
+	// Used online matrix multiplicator app, check this out =]
+	// http://www.bluebit.gr/matrix-calculator/matrix_multiplication.aspx
+	mat2 m3 = m1 * m2;
+	EXPECT_FLOAT_EQ(m3.elements[0][0], 7.37f);
+	EXPECT_FLOAT_EQ(m3.elements[0][1], 7.7f);
+	EXPECT_FLOAT_EQ(m3.elements[1][0], 15.5f);
+	EXPECT_FLOAT_EQ(m3.elements[1][1], 16.2f);
+
+	m1 *= m2;
+	EXPECT_EQ(m1.elements[0][0], m3.elements[0][0]);
+	EXPECT_EQ(m1.elements[0][1], m3.elements[0][1]);
+	EXPECT_EQ(m1.elements[1][0], m3.elements[1][0]);
+	EXPECT_EQ(m1.elements[1][1], m3.elements[1][1]);
+
+	EXPECT_FLOAT_EQ(m3.x().x, 7.37f);
+	EXPECT_FLOAT_EQ(m3.x().y, 7.7f);
+	EXPECT_FLOAT_EQ(m3.y().x, 15.5f);
+	EXPECT_FLOAT_EQ(m3.y().y, 16.2f);
 } // test_mat
+
+TEST(utils_unittest, test_rect)
+{
+
+} // test_rect
