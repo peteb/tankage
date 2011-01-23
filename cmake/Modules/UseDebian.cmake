54ed12483ddc2f949d7849ab1aeaacdcd53a1b79
Kaspars Skels <kskels@gmail.com>
2011-01-23 21:28:03 +0100

    Adding improvement comment for UseDebian CMake module

--- cmake/Modules/UseDebian.cmake
+++ cmake/Modules/UseDebian.cmake
@@ -9,6 +9,9 @@ SET(CPACK_PACKAGE_VERSION_MAJOR "0")
 SET(CPACK_PACKAGE_VERSION_MINOR "1")
 SET(CPACK_PACKAGE_VERSION_PATCH "1")
 
+
+# FIXME kaspars: Make debian package name to follow common naming convention
+# <foo>_<VersionNumber>-<DebianRevisionNumber>_<DebianArchitecture>.deb
 SET(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}")
 
 INCLUDE(CPack)
