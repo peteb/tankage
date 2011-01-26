a3ebc46a4b83eab26b102b10c261adeac575fafd
Valter <walt@apex.(none)>
2011-01-26 20:47:14 +0100

    added skeleton for AI, and modified gitignore to ignore tags and cscope.out

---
+++ src/game/ai_control.h
@@ -0,0 +1,17 @@
+#ifndef _H_CONTROL_H
+#define _H_CONTROL_H
+
+#include <game/system.h>
+
+class AIControl : public System {
+public:
+  AIControl();
+
+  void update();
+  
+private:
+};
+
+
+
+#endif
