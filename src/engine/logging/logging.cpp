321448490428ac3e0922bcda5aa0519f6ad41e3c
Kaspars Skels <kskels@gmail.com>
2011-02-07 22:35:30 +0100

    Minor log improvements

--- src/engine/logging/logging.cpp
+++ src/engine/logging/logging.cpp
@@ -6,23 +6,24 @@
 void Log::Logging::write(Logging::LogType type, const char *format, ...) {
 
   switch (type) {
-  case Logging::ERROR:
-    printf("ERROR: ");
-    break;
-  case Logging::WARNING:
-    printf("WARNING: ");
-    break;
-  case Logging::DEBUG:
-    printf("DEBUG: ");
-    break;
-  default:
-    break;
+    case Logging::ERROR:
+      printf("ER ");
+      break;
+    case Logging::WARNING:
+      printf("WA ");
+      break;
+    case Logging::DEBUG:
+      printf("DE ");
+      break;
+    default:
+      break;
   }
 
   va_list args;
   va_start (args, format);
   vprintf(format, args);
   va_end (args);
+
   printf("\n");
 }
 
