9cc215cf9e9a96372dfaf016b62427d0d482acb8
Kaspars Skels <kaspars.skels@gmail.com>
2011-03-17 23:35:54 +0100

    Added some docs for new log class

--- src/utils/log.h
+++ src/utils/log.h
@@ -1,3 +1,4 @@
+
 #ifndef UTILS_LOG_H
 #define UTILS_LOG_H
 
@@ -6,6 +7,25 @@
 #include <string>
 
 
+/**
+ * The logging interface. Logs can be printed using overloaded << operator. 
+ * If no consumer registered, all the logs will be streamed to std::cout.
+ * Log class collects the messages during it's existence and calls all the
+ * consumers in destructor, meaning, actual logging moment can be adjusted
+ * by managing log class life-cycle. See usage examples below!
+ * @code
+ * // Implement and register log consumer 
+ * struct my_log_consumer : public log::log_consumer {
+ *   void write(log::log_level level, const std::string &line) {
+ *     std::cout << line << std::endl;
+ *   }
+ * };
+ * my_log_consumer consumer;
+ * log::register_consumer(&consumer); 
+ * // Do logging and feel free to try weird combinations I haven't tested yet
+ * log(log::error) << "Very bad error occurred, exiting with code: " << -1;
+ * @endcode
+ */
 struct log {
   enum log_level {
     error,
