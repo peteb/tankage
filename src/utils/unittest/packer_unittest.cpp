61fc16d14edfd1558cc30337ebb2a8ba3fccfe7d
Kaspars Skels <kaspars.skels@gmail.com>
2011-03-26 01:01:33 +0100

    Update of Packer unittest

--- src/utils/unittest/packer_unittest.cpp
+++ src/utils/unittest/packer_unittest.cpp
@@ -5,16 +5,17 @@ TEST(PackerTest, TestBasicPackUnpack) {
   char buffer[512]; // very fast buffer allocated on stack =]
 
   Packer packer(buffer, buffer + 512);
+  packer.writeByte('t');
   packer.writeShort(17);
   packer.writeInt(42);
   packer.writeString("san-jose"); 
   EXPECT_EQ(packer.size(), 
-    sizeof(short) + sizeof(int) + sizeof(short) + sizeof(char) * 8);
+    sizeof(char) + sizeof(short) + sizeof(int) + sizeof(short) + sizeof(char) * 8);
 
   Unpacker unpacker(buffer, buffer + 512); 
+  EXPECT_EQ(unpacker.readByte(), 't');
   EXPECT_EQ(unpacker.readShort(), 17);
   EXPECT_EQ(unpacker.readInt(), 42);
-
   EXPECT_EQ(unpacker.readString(), "san-jose");
 }
 
