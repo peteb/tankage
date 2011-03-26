d3697b6c067dc5e46bac2d736aec526d905c1016
Peter Backman <ptr.bckmn@gmail.com>
2011-03-26 15:17:43 +0100

    can now store persistant state for tanks on client

---
+++ src/game/client/tank_info.h
@@ -0,0 +1,14 @@
+#ifndef GAME_CLIENT_TANK_INFO_H
+#define GAME_CLIENT_TANK_INFO_H
+
+#include <string>
+
+class TankInfo {
+public:
+  TankInfo();
+  
+  std::string name;
+};
+
+#endif // !GAME_CLIENT_TANK_INFO_H
+
