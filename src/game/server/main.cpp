758877a8a216593091c13d06c0c8f2551a5e73da
Kaspars Skels <kaspars.skels@gmail.com>
2011-04-06 21:57:15 +0200

    Cleaned-up tankage server and changed log file name

--- src/game/server/main.cpp
+++ src/game/server/main.cpp
@@ -15,38 +15,15 @@ int app_main(Portal &interfaces, const std::vector<char *> &args) {
   if (home && args.size() > 1 && args[1] && std::string(args[1]) == "-d") {
     // remove default consumer and continue to log to file
     Log::clearConsumers();
-    std::string path = std::string(home) + "/.tankage-server.conf";
+    std::string path = std::string(home) + "/.tankage-server.log";
     Log::registerConsumer(Log::ToFileConsumer(path));
   }
 
-  //WindowManager *wm = interfaces.requestInterface<WindowManager>();
-  //Graphics *gfx = interfaces.requestInterface<Graphics>();
-  
-  //wm->createWindow(800, 600);
-  
   Config config(interfaces);
   server_RegisterVariables(config);
   config.parse(args); // read and set variables from cmdline
   
   GameServer server(interfaces);
-
-  /*
-    TODO:
-      * new config should be saved
-      * unittest packer
-      * think about how removing objects will be handled (code and net)
-        * hint the client that the object is removed (send ACTORS_REMOVED packet or similar)
-        * client can remove object if not updated within a set time
-        * if a hint was wrong (started to receive updates), recreate the object
-      * start using packer instead of structs
-      * start using new log
-      * 
-   
-   */
-  //const rect wndSize = wm->size();
-  //gfx->setViewport(wndSize);
-  //gfx->setOrtho(wndSize);
-  //wm->swapBuffers(); 
 
   if (args.size() > 1 && args[1] && std::string(args[1]) == "-d") {
     pid_t pid = fork();
@@ -57,6 +34,7 @@ int app_main(Portal &interfaces, const std::vector<char *> &args) {
       throw std::runtime_error("failed to fork");
     }
   }
+
   // child continues
   server.run();
 
