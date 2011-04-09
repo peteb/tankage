1b46d1d73a1d843727b35f8e530066e6fd485554
Peter Backman <ptr.bckmn@gmail.com>
2011-04-09 10:59:57 +0200

    forgot to add platform I guess

---
+++ src/platform/opengl/graphics.h
@@ -0,0 +1,37 @@
+#ifndef PLATFORM_OPENGL_GRAPHICS_H
+#define PLATFORM_OPENGL_GRAPHICS_H
+
+#include <platform/graphics.h>
+#include <utils/singleton.h>
+#include <utils/rect.h>
+#include <vector>
+
+namespace OpenGl {
+class Graphics : public ::Graphics, public Singleton<Graphics> {
+public:
+  class Texture *createTexture(class Image *image);
+
+  void clear(const class color4 &clearColor);
+  void drawQuad(const class rect &quad, float dir);
+  void drawQuad(const class rect &quad, const class rect &source);
+  void drawQuads(const std::vector<rect> &quads);
+  void drawQuads(unsigned components, unsigned vertices, float *coord, float *tc);
+  
+  void drawLinestrip(const std::vector<vec2> &lines);
+  
+  void drawCircle(const class vec2 &pos, float radius, float tess);
+  
+  void setTransform(const class vec2 &translation);
+  void setOrtho(const class rect &size);
+  void setViewport(const class rect &size);
+  void setBlend(BlendMode mode);
+  void setColor(const color4 &col);
+  void setTexture(class Texture *texture);
+  
+  void enableTextures();
+  void disableTextures();
+};
+
+}
+
+#endif // !PLATFORM_OPENGL_GRAPHICS_H
